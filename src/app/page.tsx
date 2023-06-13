eef8c5a85120e5fdb5a0cc5980e11da8c7e99eb7
tegarjgap <tegarjagatgeni@gmail.com>
2023-06-13 22:40:15 +0700

    feat: add pwa configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,36 @@
+import { Metadata } from 'next';
 import Image from 'next/image'
+
+
+const APP_NAME = "next-pwa example";
+const APP_DESCRIPTION = "This is an example of using next-pwa plugin";
+ 
+export const metadata: Metadata = {
+  title: "PWA 💖 Next.js",
+  description: APP_DESCRIPTION,
+  twitter: {
+    card: "summary_large_image",
+    images: "https://example.com/og.png",
+  },
+  applicationName: APP_NAME,
+  appleWebApp: {
+    capable: true,
+    title: APP_NAME,
+    statusBarStyle: "default",
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  themeColor: "#FFFFFF",
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  manifest: "/manifest.json",
+  icons: [
+    { rel: "apple-touch-icon", url: "/icons/apple-touch-icon.png" },
+    { rel: "shortcut icon", url: "/favicon.ico" },
+  ],
+  keywords: ["nextjs", "pwa", "next-pwa"],
+};
 
 export default function Home() {
   return (
